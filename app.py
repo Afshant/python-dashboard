535a40f209314b7e1de9b31729005ec51b556e1d
Afshant <afshant213@gmail.com>
2024-10-21 13:44:45 +0530

    data analysis

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Sample data-fetching function
 def fetch_data():
     # Example: Use an API or local CSV
-    api_url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=461dd2ad084f12a253366ce3e3b8370b'
+    api_url = 'https://api.openweathermap.org/data/2.5/weather?q==London&appid=YOUR_API_KEY_HERE'
     response = requests.get(api_url)
     json_data = response.json()
     df = pd.json_normalize(json_data)
